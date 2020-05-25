85f62e00ece4aca1b27f2722ee4b46dbda0d9948
Hellowalls1 <thesingingbutcher@gmail.com>
2020-05-25 14:44:49 -0500

    removed arrays, added section titles

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,17 +1,18 @@
 function main() {
+  console.log("All Place Names")
   const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
     "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
   ]
-  console.log(names)
 
   for (let name of names) {
     console.log(name)
   }
-  console.log()
 
+  console.log("")
+
+  console.log("'The' Place Names")
   const newNames = names.filter((n) => n.startsWith("The"))
 
-  console.log(newNames)
 
   for (let n of newNames) {
     console.log(n)

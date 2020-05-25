8a645bac0bdea769e381b93417655de042ef5700
Hellowalls1 <thesingingbutcher@gmail.com>
2020-05-25 14:09:05 -0500

    exercies

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,21 @@
 function main() {
-  // Put your code here
+  const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+  ]
+  console.log(names)
 
+  for (let name of names) {
+    console.log(name)
+  }
+  console.log()
+
+  const newNames = names.filter((n) => n.startsWith("The"))
+
+  console.log(newNames)
+
+  for (let n of newNames) {
+    console.log(n)
+  }
 }
 
 main();

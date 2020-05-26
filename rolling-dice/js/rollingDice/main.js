30b0531c52e9e9167eca00df0624578877ef21ef
Hellowalls1 <thesingingbutcher@gmail.com>
2020-05-26 11:49:43 -0500

    started to convert

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,5 +1,16 @@
 function main() {
-  // Put your code here
+  console.log("Let's roll some dice, baby!")
+  console.log("-------------")
+  for (let i = 0; i < 10; i++) {
+    let die1 = Roll()
+    let die2 = Roll();
+
+    const message = `(${die1} + ${die2}) === (${die1.value} + {die2.value})`
+    if (die1.value === die2.value) {
+      message = " DOUBLES!";
+    }
+    console.log(message)
+  }
 
 }
 

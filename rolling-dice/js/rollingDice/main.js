d9a58ecfddc0d47b7fc3f4ad41204addca6dd055
Hellowalls1 <thesingingbutcher@gmail.com>
2020-05-26 19:21:34 -0500

    still working

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -11,7 +11,48 @@ function main() {
     }
     console.log(message)
   }
-
 }
 
+let Roll = () => {
+  getRandomInt(1, 6) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  const Die = (value) => {
+    return {
+      Value: value
+    }
+  }
+
+  let dieString = parseInt(Die)
+
+
+  switch (Value) {
+    case 1:
+      dieString = "\u2680";
+      break;
+    case 2:
+      dieString = "\u2681";
+      break;
+    case 3:
+      dieString = "\u2682";
+      break;
+    case 4:
+      dieString = "\u2683";
+      break;
+    case 5:
+      dieString = "\u2684";
+      break;
+    case 6:
+      dieString = "\u2685";
+      break;
+  }
+  return dieString
+}
+
+
+
+  }
 main();
